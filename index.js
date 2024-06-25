c34a3e7a07a8fd0adda0707cd646a633a86b0ee4
Johnson Kow <jkow95@gmail.com>
2024-06-25 16:30:34 -0400

    Connect to Vercel

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// import pg from "pg";
+// const { Pool } = pg;
+
 const express = require("express");
 const { Pool } = require("pg");
 const cors = require("cors");
@@ -19,12 +22,16 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 //Create a PostgreSQL connection pool
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   port: process.env.DB_PORT,
+// });
+
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT,
+  connectionString: process.env.POSTGRES_URL,
 });
 
 /* This code snippet is defining a route handler for updating a specific ticket in the database.*/
